f25423f7c78a4968ac0802b63fa51ae8718fe2a2
Tom Robinson <tarobins@gmail.com>
2023-12-05 07:49:40 +0000

    Day 4 part 1

--- 4/4a.py
+++ 4/4a.py
@@ -2,8 +2,17 @@ from absl import app
 
 def main(argv):
     filename = argv[1]
-    print(filename)
-
+    sum = 0
+    with open(filename, 'r') as f:
+        for line in f:
+            card_content = line.split(':')[1]
+            winning_numbers = list(filter(lambda x: x != '', card_content.split('|')[0].strip().split(' ')))
+            my_numbers = list(filter(lambda x: x != '', card_content.split('|')[1].strip().split(' ')))
+            intersection_count = len(set(winning_numbers) & set(my_numbers))
+            if intersection_count > 0:
+                sum += 2 ** (intersection_count - 1)
+    
+    print(sum)
 
 if __name__ == '__main__':
     app.run(main)
