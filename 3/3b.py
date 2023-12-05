7d556436dad24f3dd1464140ee9a1325edbfcd11
Tom Robinson <tarobins@gmail.com>
2023-12-05 07:29:46 +0000

    Day 3

--- 3/3b.py
+++ 3/3b.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 def main(argv):
-    filename = '3/test_data.txt'
+    filename = argv[1]
 
     with open(filename, 'r') as f:
         lines = f.readlines()
@@ -35,46 +35,17 @@ def main(argv):
             else:
                 col = col + 1
 
-    
-
-
-    # n = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
-
-    # print(n)
-
-    # value_array = np.full(number_array.shape, None)
-
-    # value_array[0][0] = 
-
-    # symbol_array = np.where(np.logical_and(np.logical_not(np.char.isdigit(char_arr)), np.logical_not(char_arr == '.')), char_arr, None)
-    
-    # neighbor_array = np.full(symbol_array.shape, None)
-    
-    # for row in range(symbol_array.shape[0]):
-    #     for col in range(symbol_array.shape[1]):
-    #         if symbol_array[row][col] is not None:
-    #             neighbor_array[max(0, row-1):min(row+2, symbol_array.shape[0]), 
-    #                            max(0, col-1):min(col+2, symbol_array.shape[1])] = symbol_array[row][col]
-    part_sum = 0
-    # for row in range(symbol_array.shape[0]):
-    #     col = 0
-    #     while col < symbol_array.shape[1]:
-    #         if char_arr[row][col].isdigit():
-    #             isPart = neighbor_array[row][col] is not None
-    #             num = int(char_arr[row][col])   
-    #             col = col + 1
-    #             while col < symbol_array.shape[1] and char_arr[row][col].isdigit():
-    #                 isPart = isPart or neighbor_array[row][col] is not None
-    #                 num = num * 10 + int(char_arr[row][col])
-    #                 col = col + 1
-    #             if isPart:
-    #                 part_sum = part_sum + num
-    #         else:
-    #             col = col + 1
-    #     row = row + 1
-    
-    # print(part_sum) 
-
+    # find the *s
+    sum = 0
+    for row in range(char_arr.shape[0]):
+        for col in range(char_arr.shape[1]):
+            if char_arr[row][col] == '*':
+                neighbor_ids = id_array[max(0, row-1):min(row+2, id_array.shape[0]), max(0, col-1):min(col+2, id_array.shape[1])]
+                neighbor_ids = neighbor_ids[neighbor_ids != None]
+                unique_neighbor_ids = np.unique(neighbor_ids)
+                if len(unique_neighbor_ids) == 2:
+                    sum = sum + number_lookup[unique_neighbor_ids[0]] * number_lookup[unique_neighbor_ids[1]]
+    print(sum)     
 
 if __name__ == '__main__':
     app.run(main)
