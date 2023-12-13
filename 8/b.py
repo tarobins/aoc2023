aba1e07cc4d883076baecb424c360e45f26974cd
Tom Robinson <tarobins@gmail.com>
2023-12-13 01:00:09 +0000

    Day 8 part 2

--- 8/b.py
+++ 8/b.py
@@ -1,4 +1,5 @@
 from absl import app
+import numpy as np
 
 def main(argv):
     filename = argv[1]
@@ -18,33 +19,27 @@ def main(argv):
             right = value[6:9]
             states[state] = [left, right]
 
-    transitions = 0
-    current_direction_index = 0
-    current_states = [key for key in states.keys() if key.endswith('A')]
-    current_states = ['DBA']
-    number_of_states = len(current_states)
-    
-    while len([key for key in current_states if key.endswith('Z')]) != number_of_states:
-        print(f'{current_states} {len([key for key in current_states if key.endswith("Z")])}')
-        new_states = []
-        for state in current_states:
-            options = states[state]
-            if directions[current_direction_index] == 'L':
-                new_states.append(options[0])
-            else:
-                new_states.append(options[1])
-        current_direction_index = (current_direction_index + 1) % len(directions)
-        transitions += 1
-        current_states = new_states
-    #     options = states[current_state]
-    #     if directions[current_direction_index] == 'L':
-    #         current_state = options[0]
-    #     else:
-    #         current_state = options[1]
-    #     current_direction_index = (current_direction_index + 1) % len(directions)
-    #     transitions += 1
+    lcm = 1
 
-    print(transitions)
+    start_states = [key for key in states.keys() if key.endswith('A')]
+    
+    for start in start_states:
+        transitions = 0
+        current_direction_index = 0
+        current_state = start
+        while not current_state.endswith('Z'):
+            options = states[current_state]
+            if directions[current_direction_index] == 'L':
+                current_state = options[0]
+            else:
+                current_state = options[1]
+            current_direction_index = (current_direction_index + 1) % len(directions)
+            transitions += 1
+        print(transitions)
+        lcm = np.lcm(lcm, transitions)
+    
+    print(lcm)
+    
 
 if __name__ == '__main__':
     app.run(main)
